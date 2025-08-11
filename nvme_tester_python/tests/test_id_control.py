9aaa10359a20bcadeb1330344cbaf6dcb3eac77f
magicking2108 <adan.baltazar7746@alumnos.udg.mx>
2025-08-10 19:39:54 -0600

    test

--- nvme_tester_python/tests/test_id_control.py
+++ nvme_tester_python/tests/test_id_control.py
@@ -2,47 +2,67 @@ import json
 from src.nvme_wrapper import NvmeCommands
 from src.logger import TestLogger
 
+DEVICE = "/dev/nvme0"
+NVME = "nvme"
+
 class NvmeIdCtrlTest:
-    def __init__(self, nvme_wrapper, logger):
-        self.nvme = nvme_wrapper
-        self.logger = logger
-    
+    def __init__(self, device=DEVICE, nvme_cli=NVME):
+        #self.nvme = nvme_wrapper
+        #self.logger = logger
+        self.device = device
+        self.nvme_cli = nvme_cli
+
+    def idctrol(self, **kwargs):
+        cmd = [
+            self.nvme_cli,
+            "id-ctrl",
+            self.device
+        ]
+        options = self.parametrizeOpcionsLogs(**kwargs)
+        cmd.extend(options)
+
+        output = self.run_command(cmd)
+
+        if kwargs.get('json_output', False) and output:
+            output = json.loads(output)
+        #return output
+
     def run(self, reference_file_path):
-        self.logger.log_test_start("test_id_ctrl")
-        
-        output = self.nvme.idctrol(json_output=True)
+        # self.logger.log_test_start("test_id_ctrl")
+
+        output = self.idctrol(json_output=True)
         if output is None:
-            self.logger.log_test_end("test_id_ctrl", "FAIL")
+            # self.logger.log_test_end("test_id_ctrl", "FAIL")
             return False
-        
+
         with open(reference_file_path, "r") as f:
             reference = json.load(f)
-        
+
         ignore_fields = ["sn", "fguid", "unvmcap", "subnqn"]
         errors = 0
-        
+
         for key, expected_value in reference.items():
             if key in ignore_fields:
                 continue
             actual_value = output.get(key)
             if actual_value != expected_value:
                 errors += 1
-                self.logger.error(f"Error en campo '{key}': esperado '{expected_value}', encontrado '{actual_value}'")
+                # self.logger.error(f"Error en campo '{key}': esperado '{expected_value}', encontrado '{actual_value}'")
             else:
-                self.logger.info(f"Campo '{key}' coincide: {actual_value}")
-        
+                # self.logger.info(f"Campo '{key}' coincide: {actual_value}")
+                pass
+
         if errors == 0:
-            self.logger.log_test_end("test_id_ctrl", "PASS")
+            # self.logger.log_test_end("test_id_ctrl", "PASS")
             return True
         else:
-            self.logger.log_test_end("test_id_ctrl", "FAIL")
+            # self.logger.log_test_end("test_id_ctrl", "FAIL")
             return False
-
 
 # --- Esto es para testear el archivo directamente ---
 
 if __name__ == "__main__":
-    logger = TestLogger("id_ctrl_test").initialize_logger()
-    nvme = NvmeCommands(logger)
-    test = NvmeIdCtrlTest(nvme, logger)
+   # logger = TestLogger("id_ctrl_test").initialize_logger()
+   # nvme = NvmeCommands(logger)
+    test = NvmeIdCtrlTest()
     test.run("tests/id-ctrl-main.json")  # Ruta relativa al JSON de referencia
