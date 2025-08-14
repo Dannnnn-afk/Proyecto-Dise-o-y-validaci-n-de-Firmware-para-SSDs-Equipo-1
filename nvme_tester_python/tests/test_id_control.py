e71e5fb6fa778a0f9756a7cd1d05a4e0c1aec47a
Fabian-J-Escalante <bladebreakerr@gmail.com>
2025-08-13 22:11:13 -0600

    [Fix] Test 2 can't find json util. Issue related to use of os

--- nvme_tester_python/tests/test_id_control.py
+++ nvme_tester_python/tests/test_id_control.py
@@ -2,8 +2,8 @@ import json
 import sys
 import os
 
-# Add the parent directory to the path to import modules
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Path to read the json from anywhere
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "idcontrol.json")
 
 from src.nvme_wrapper import NvmeCommands
 from src.logger import TestLogger
